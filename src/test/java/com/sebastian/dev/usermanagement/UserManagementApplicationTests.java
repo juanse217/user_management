5cbf92078b3bf37449cf7e107cab6dc24027eb81
juanse217 <juansebas08004@gmail.com>
2026-03-30 14:23:02 -0500

    initial commit

---
+++ src/test/java/com/sebastian/dev/usermanagement/UserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebastian.dev.usermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
